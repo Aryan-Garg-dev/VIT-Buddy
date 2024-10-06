405a97ff3dbe0ff347abf522eea6073a684d3290
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-06 10:55:28 +0530

    test

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,4 +1,4 @@
-import { DEFAULT_DESCRIPTION, DEFAULT_KEYWORDS, DEFAULT_LOGO, DEFAULT_SOCIAL_IMG_URL, DEFAULT_TITLE } from "@/constants/meta"
+import { DEFAULT_DESCRIPTION, DEFAULT_KEYWORDS, DEFAULT_LOGO, DEFAULT_TITLE } from "@/constants/meta"
 import { MetaData } from "@/constants/SEOContent"
 import { Helmet } from "react-helmet-async"
 
@@ -7,10 +7,6 @@ export const SEO = ({
   description = DEFAULT_DESCRIPTION, 
   keywords = DEFAULT_KEYWORDS,
   imageURL = DEFAULT_LOGO,
-  socialURL,
-  socialImgURL = DEFAULT_SOCIAL_IMG_URL,
-  socialTitle = DEFAULT_TITLE,
-  socialDescription = DEFAULT_DESCRIPTION
 }: MetaData)=>{
   return (
     <Helmet>
@@ -19,25 +15,6 @@ export const SEO = ({
       <meta name="keywords" content={keywords} />
       <meta name="robots" content="index,follow" />
       <link rel="icon" type="image/webp" href={imageURL} />
-      {socialURL && <link rel="canonical" href={socialURL} />}
-
-      {/* Social Meta Tags */}
-      {socialURL && (
-        <>
-          <meta property="og:title" content={socialTitle} />
-          <meta property="og:description" content={socialDescription} />
-          <meta property="og:image" content={socialImgURL} />
-          <meta property="og:url" content={socialURL} />
-          <meta property="og:type" content="website" />
-          <meta property="og:site_name" content="VIT Buddy" />
-
-          {/* Twitter Meta Tags */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content={socialTitle} />
-          <meta name="twitter:description" content={socialDescription} />
-          <meta name="twitter:image" content={socialImgURL} />
-        </>
-      )}
     </Helmet>
   )
 }
--- src/constants/SEOContent.ts
+++ src/constants/SEOContent.ts
@@ -1,15 +1,10 @@
-import { DEFAULT_DESCRIPTION, DEFAULT_KEYWORDS, DEFAULT_TITLE, PRODUCTION_URL } from "./meta"
+import { DEFAULT_DESCRIPTION, DEFAULT_KEYWORDS, DEFAULT_TITLE } from "./meta"
 
 export type MetaData = Partial<{
   title: string,
   keywords: string,
   description: string,
   imageURL: string,
-
-  socialURL: string,
-  socialImgURL: string,
-  socialTitle: string,
-  socialDescription: string,
 }>
 
 export type PageMetaData = {
@@ -22,11 +17,6 @@ export const SEOContent: PageMetaData  = {
     description: DEFAULT_DESCRIPTION,
     keywords: DEFAULT_KEYWORDS,
     imageURL: "/images/hack.webp",
-
-    socialImgURL: PRODUCTION_URL + "/images/large/hack.webp",
-    // socialURL: PRODUCTION_URL,
-    socialTitle: DEFAULT_TITLE,
-    socialDescription: "Streamline your academic journey with VIT Buddy. Plan your FFCS, calculate your CGPA, and track attendance effortlessly. Perfect for VIT students!"
   },
 
   notFound: {
@@ -34,18 +24,11 @@ export const SEOContent: PageMetaData  = {
     description: DEFAULT_DESCRIPTION,
     keywords: DEFAULT_KEYWORDS,
     imageURL: "/images/hack.webp",
-
-    socialImgURL: PRODUCTION_URL + "/images/large/hack.webp",
-    socialTitle: "Page not found • VIT Buddy"
   },
 
   cgpa: {
     title: "CGPA Calculator • VIT Buddy",
     imageURL: "/images/cgpa.webp",
-
-    socialImgURL: PRODUCTION_URL + "/images/large/cgpa.webp",
-    // socialURL: PRODUCTION_URL + "/services/cgpa",
-    socialTitle: "CGPA Calculator • VIT Buddy",
   },
 
   gpa: {
@@ -53,29 +36,15 @@ export const SEOContent: PageMetaData  = {
     description: "Easily calculate your GPA with our user-friendly GPA calculator. Track your academic performance, manage your grades, and stay on top of your progress. Perfect for students aiming for success.",
     imageURL: "/images/gpa.webp",
     keywords: "gpa, vit, vellore, chennai, ap, bangalore, cgpa, calculator, acadmemic, progress, students, success",
-
-    socialImgURL: PRODUCTION_URL + "/images/large/gpa.webp",
-    // socialURL: PRODUCTION_URL + "/services/gpa",
-    socialDescription: "Use our easy-to-use GPA calculator to calculate your GPA, manage your grades, and monitor your academic progress. Stay on track towards your goals!",
-    socialTitle: "GPA Calculator • VIT Buddy"
   },
 
   ffcs: {
     title: "FFCS Planner • VIT Buddy",
     imageURL: "/images/ffcs.webp",
-
-    socialImgURL: PRODUCTION_URL + "/images/large/ffcs.webp",
-    // socialURL: PRODUCTION_URL + "/services/ffcs-planner",
-    socialTitle: "FFCS Planner • VIT Buddy"
   },
 
   attendance: {
     title: "Attendance Calculator • VIT Buddy",
-    imageURL: "/images/attendance.webp",
-
-    socialImgURL: PRODUCTION_URL + "/images/large/attendance.webp",
-    // socialURL: PRODUCTION_URL + "/services/attendance",
-    socialTitle: "Attendance Calculator • VIT Buddy"
-    
+    imageURL: "/images/attendance.webp",  
   }
 } 
