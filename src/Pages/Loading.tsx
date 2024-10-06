6b2993e6bde7143b56f3cf1aadf42f6983e14d50
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-06 10:24:54 +0530

    fix

--- src/Pages/Loading.tsx
+++ src/Pages/Loading.tsx
@@ -8,7 +8,7 @@ export const Loading = ()=>{
     <div className="min-h-screen w-full flex flex-col justify-center items-center space-y-2">
       <Ripple className="h-full w-full">
         <div className="p-5 rounded-3xl flex flex-col justify-center items-center">
-          <div className="animate-pulse mb-2"><Logo src={hack} size={"medium"} /></div>
+          <div className="animate-pulse mb-2"><Logo src={hack} size={"medium"} alt={"hack"} /></div>
           <div className="flex justify-center items-center gap-3">
             <Spinner size={"lg"} />
             <span className="font-regular text-2xl md:text-3xl">Loading</span>
