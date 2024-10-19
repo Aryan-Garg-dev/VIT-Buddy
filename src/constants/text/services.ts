e34f9fc3defa25f182f4a1fbfa1cfdd438d13b91
Aryan-Garg-dev <148696092+Aryan-Garg-dev@users.noreply.github.com>
2024-10-20 01:05:19 +0530

    update: cgpa-status

--- src/constants/text/services.ts
+++ src/constants/text/services.ts
@@ -11,7 +11,7 @@ type Service = {
 
 export const services: Service[] = [
   { title: "GPA Calculator", description: description.gpa, imageURL: gpa, link: links.gpa },
-  { title: "CGPA Calculator", description: description.cgpa, imageURL: cgpa, link: links.cgpa, status: "Coming Soon" },
+  { title: "CGPA Calculator", description: description.cgpa, imageURL: cgpa, link: links.cgpa },
   { title: "FFCS Planner", description: description.ffcs, imageURL: ffcs, link: links.ffcs, status: "Coming Soon" },
   { title: "Attendance Calculator", description: description.attendance, imageURL: attendance, link: links.attendance, status: "Coming Soon" },
 ]
